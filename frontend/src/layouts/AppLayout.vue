afb4865808ef90e770d11da143f5642a750a856c
Your Name <you@example.com>
2026-04-08 11:24:21 +0700

    Stabilize header layout with scrollbars

--- frontend/src/layouts/AppLayout.vue
+++ frontend/src/layouts/AppLayout.vue
@@ -2,7 +2,7 @@
   <div class="min-h-screen overflow-x-hidden bg-[#f7f9ff] text-slate-900 lg:flex">
     <AppSidebar :open="sidebarOpen" @close="sidebarOpen = false" />
 
-    <div class="flex min-h-screen flex-1 flex-col lg:ml-[270px]">
+    <div class="app-shell-content flex min-h-screen flex-1 flex-col lg:ml-[270px]">
       <AppHeader @toggle-sidebar="sidebarOpen = !sidebarOpen" />
       <main class="flex-1 bg-[#f4f7ff] px-4 py-5 sm:px-6 lg:px-8">
         <RouterView />
@@ -20,3 +20,12 @@ import AppSidebar from '../components/AppSidebar.vue'
 
 const sidebarOpen = ref(false)
 </script>
+
+<style scoped>
+@media (min-width: 1024px) {
+  .app-shell-content {
+    width: calc(100vw - 270px);
+    max-width: calc(100vw - 270px);
+  }
+}
+</style>
